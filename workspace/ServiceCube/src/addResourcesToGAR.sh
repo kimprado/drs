90942e1f9869bc82020a0b89dc8f5147532cf8aa
Kim Prado <kimprado21@gmail.com>
2012-03-06 00:22:17 -0300

    Correção do acaminho de jar.

--- workspace/ServiceCube/src/addResourcesToGAR.sh
+++ workspace/ServiceCube/src/addResourcesToGAR.sh
@@ -4,7 +4,7 @@ mkdir temp_dir_AddResourcesToGAR
 rm d_org_cube_service_added.gar
 
 unzip -d ./temp_dir_AddResourcesToGAR  org_cube_service.gar
-jar uf temp_dir_AddResourcesToGAR/lib/org_cube_service.jar META-INF/persistence.xml
+$JAVA_HOME/bin/jar uf temp_dir_AddResourcesToGAR/lib/org_cube_service.jar META-INF/persistence.xml
 
 #pwd
 
